bf222670cbed5914586e96d68bc3f7fc04350f1a
aplefull <mraplefull@yandex.by>
2022-04-23 19:36:52 +0300

    add index route

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ const fetch = require('node-fetch');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+const VALID_URL_REGEX = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+(:[0-9]+)?|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/;
 
 app.use(express.json());
 app.use(cors({
     origin: '*',
 }));
+
+app.get('/', (_, res) => {
+    res.status(200).send('Service is up!');
+});
 
 app.all('*', async (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
@@ -21,7 +26,12 @@ app.all('*', async (req, res) => {
     }
 
     const targetURL = req.url.replace('/', '');
-    console.log(`Fetching ${targetURL}`);
+
+    if (!VALID_URL_REGEX.test(targetURL)) {
+        res.status(400).send('Invalid URL');
+        return;
+    }
+
     const response = await fetch(targetURL, {
         method: req.method,
         json: req.body,
